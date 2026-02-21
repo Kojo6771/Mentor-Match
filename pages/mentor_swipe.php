c94780c500779cc18ee3b8e6f69780f9275ef8dc
Kojo6771 <230165858@aston.ac.uk>
2026-02-21 02:21:47 +0000

    Remove back link from mentor swipe page
    Delete the 'Back to Dashboard' anchor (including its SVG icon) from pages/mentor_swipe.php to streamline the mentor swipe interface and reduce redundant navigation options.

--- pages/mentor_swipe.php
+++ pages/mentor_swipe.php
@@ -159,13 +159,6 @@ try {
 <body>
     <main class="container">
         <div class="swipe-wrapper">
-            <a href="dashboard.php" class="back-link">
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                    <path d="M19 12H5M12 19l-7-7 7-7"/>
-                </svg>
-                Back to Dashboard
-            </a>
-
             <div class="page-header">
                 <h1>Find Your Mentor</h1>
                 <p>Swipe right to connect, left to pass</p>
